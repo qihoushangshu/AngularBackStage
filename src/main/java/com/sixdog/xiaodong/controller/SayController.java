406fe9f8cf8ded3034b0fc40104247a2986bf4d0
司小东 <sixd163@163.com>
2020-03-15 15:20:04 +0900

    say保存到文件，保存成功提示增加

--- src/main/java/com/sixdog/xiaodong/controller/SayController.java
+++ src/main/java/com/sixdog/xiaodong/controller/SayController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sixdog.xiaodong.dto.SayInfo;
+import com.sixdog.xiaodong.util.CSVUtil;
+
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +27,15 @@ public class SayController {
 		logger.info("--------------------------start");
 	    logger.info(say.getTitle());
 	    logger.info(say.getContent());
+	    String uuid = UUID.randomUUID().toString();
+	    try {
+			CSVUtil.createCSV(new String[] {uuid,say.getTitle(),say.getContent()});
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			logger.info(e.getMessage());
+		}
 	    logger.info("--------------------------end");
-	    return "{\"a\":\"成功1\"}";
+	    return "{\"ret\":\"success\"}";
 	}
 	
 	@RequestMapping(path = "/test")
